70ca88a710d13ca123d603f6d286f4a63c8f45c2
Harshita Yadav <132802431+HarshuYs124@users.noreply.github.com>
2026-04-04 19:59:41 +0530

    Assesment4

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,26 +4,26 @@ from utils.loggers import get_logger
 
 
 def test_valid_login(setup_and_teardown):
-    driver = setup_and_teardown
+    driver = setup_and_teardown # get driver instance from fixture
 
-    lp = LoginPage(driver)
+    lp = LoginPage(driver) # create loginpage object
 
-    config = ConfigReader.read_config()
-    env = config['qa']
+    config = ConfigReader.read_config() # read configuration from YAML
+    env = config['qa'] # select qa environment
 
-    BASE_URL = env['base_url']
-    USERNAME = env['username']
-    PASSWORD = env['password']
+    BASE_URL = env['base_url'] # get application URL
+    USERNAME = env['username'] # get username
+    PASSWORD = env['password'] # get password
 
-    # Open the website
+    # opening the website using base URL
     driver.get(BASE_URL)
 
-    get_logger().info("Trying to log in")
+    get_logger().info("Trying to log in") # log info message
     # get_logger().error()
 
 
     # Perform login
-    lp.click_login()
-    lp.enter_email(USERNAME)
-    lp.enter_password(PASSWORD)
-    lp.click_login_button()
+    lp.click_login() # click on login link
+    lp.enter_email(USERNAME) # enter username/email
+    lp.enter_password(PASSWORD) # enter password
+    lp.click_login_button() # click login button
